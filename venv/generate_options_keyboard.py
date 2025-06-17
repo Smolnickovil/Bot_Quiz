efc37e4e99f76cffb5c97d1b5b6793e40e488a84
Smolnikov Ilya <125181932+Smolnickovil@users.noreply.github.com>
2025-06-17 21:49:37 +0700

    Bot_Quiz_v1

---
+++ venv/generate_options_keyboard.py
@@ -0,0 +1,21 @@
+#@title Полный код бота для самоконтроля
+import aiosqlite
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
+from aiogram import F
+
+
+def generate_options_keyboard(answer_options, right_answer):
+    builder = InlineKeyboardBuilder()
+
+    for option in answer_options:
+        builder.add(types.InlineKeyboardButton(
+            text=option,
+            callback_data="right_answer" if option == right_answer else "wrong_answer")
+        )
+
+    builder.adjust(1)
+    return builder.as_markup()
