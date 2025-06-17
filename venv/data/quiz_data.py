4bfb3b8fd757324699f2b95cc1e0a68d303cc1d3
Smolnikov Ilya <125181932+Smolnickovil@users.noreply.github.com>
2025-06-17 21:55:38 +0700

    Update quiz_data.py

--- venv/data/quiz_data.py
+++ venv/data/quiz_data.py
@@ -10,5 +10,49 @@ quiz_data = [
         'options': ['int', 'float', 'str', 'natural'],
         'correct_option': 0
     },
-    # Добавьте другие вопросы
+    {
+        'question': 'Как называется метод класса, который возвращает строку представления объекта?', 
+        'options': ["__repr__()", "__init__()", "__del__()"], 
+        'correct_option': 0
+    },
+    {
+        'question': 'Какой оператор используется для проверки равенства двух значений?', 
+        'options': ["==", "=", "!="], 
+        'correct_option': 0
+    },
+    {
+        'question': 'Какие типы данных используются для хранения строковых данных?', 
+        'options': ["str", "int", "bool"], 
+        'correct_option': 0
+    },
+    {
+        'question': 'Что означает аббревиатура SQL?', 
+        'options': ["Structured Query Language", "Simple Question Logic", "Sequential Queries Library"], 
+        'correct_option': 0
+    },
+    {
+        'question': 'Название первого браузера интернета было:', 
+        'options': ["Netscape Navigator", "Internet Explorer", "WorldWideWeb"], 
+        'correct_option': 2
+    },
+    {
+        'question': 'Где была разработана первая версия языка JavaScript?', 
+        'options': ["Microsoft", "Sun Microsystems", "Netscape Communications"], 
+        'correct_option': 2
+    },
+    {
+        'question': 'Самая популярная операционная система для серверов — это:', 
+        'options': ["Windows Server", "Linux", "macOS"], 
+        'correct_option': 1
+    },
+    {
+        'question': 'В каком году впервые появился термин „Интернет“?', 
+        'options': ["1983", "1969", "1995"], 
+        'correct_option': 1
+    },
+    {
+        'question': 'Первый компьютер назывался:', 
+        'options': ["ENIAC", "UNIVAC I", "IBM PC"], 
+        'correct_option': 0
+    }
 ]
